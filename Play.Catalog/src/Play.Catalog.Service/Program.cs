b7e03bf5eaadaffa9642799dea52f0e88fc4e75a
Sheersendu <shibrata15@gmail.com>
2024-12-14 17:16:59 +0530

    Refactored MongoDB registration into extension methods

--- Play.Catalog/src/Play.Catalog.Service/Program.cs
+++ Play.Catalog/src/Play.Catalog.Service/Program.cs
@@ -1,7 +1,3 @@
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization;
-using MongoDB.Bson.Serialization.Serializers;
-using MongoDB.Driver;
 using Play.Catalog.Service.Entities;
 using Play.Catalog.Service.Repositories;
 using Play.Catalog.Service.Settings;
@@ -9,27 +5,15 @@ using Play.Catalog.Service.Settings;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 
-var serviceSettings = builder.Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
-
 builder.Services.AddControllers(options =>
 {
 	options.SuppressAsyncSuffixInActionNames = false;
 });
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton(serviceProvider =>
-{
-	var mongoDbSettings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-	var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
-	return mongoClient.GetDatabase(serviceSettings.ServiceName);
-});
-builder.Services.AddSingleton<IRepository<Item>>(serviceProvider =>
-{
-	var database = serviceProvider.GetService<IMongoDatabase>();
-	return new MongoRepository<Item>(database, "items");
-});
 
-BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
-BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+var serviceSettings = builder.Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+builder.Services.AddMongo()
+				.AddMongoRepository<Item>("items");
 
 var app = builder.Build();
 
