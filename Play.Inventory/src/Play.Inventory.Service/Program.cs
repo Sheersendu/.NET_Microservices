0caefa5c717f0536d64fe2d142453d9f98390e55
Sheersendu <shibrata15@gmail.com>
2024-12-19 19:50:22 +0530

    Added retries with exponential backoff

--- Play.Inventory/src/Play.Inventory.Service/Program.cs
+++ Play.Inventory/src/Play.Inventory.Service/Program.cs
@@ -1,6 +1,8 @@
 using Play.Common.MongoDB;
 using Play.Inventory.Service.Clients;
 using Play.Inventory.Service.Entities;
+using Polly;
+using Polly.Timeout;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,14 +12,20 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 builder.Services.AddMongo()
 				.AddMongoRepository<InventoryItem>("inventoryitems");
+Random jitterer = new Random();
 builder.Services.AddHttpClient<CatalogClient>(client =>
 {
 	client.BaseAddress = new Uri("http://localhost:5103");
-});
+})
+.AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(
+	5,
+	retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)) // Exponential backoff
+					+ TimeSpan.FromMilliseconds(jitterer.Next(0, 1000)) // Random jitter
+))
+.AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(TimeSpan.FromSeconds(1)));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
