37d54ce98bd5d54deb36d425bb2ab50fa816d9ce
Sheersendu <shibrata15@gmail.com>
2024-12-21 16:20:54 +0530

    Added circuit breaker policy to HTTP client for improved resilience

--- Play.Inventory/src/Play.Inventory.Service/Program.cs
+++ Play.Inventory/src/Play.Inventory.Service/Program.cs
@@ -16,9 +16,13 @@ builder.Services.AddHttpClient<CatalogClient>(client =>
 	client.BaseAddress = new Uri("http://localhost:5103");
 })
 .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(
-	5,
+	5, // no of retries
 	retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)) // Exponential backoff
 					+ TimeSpan.FromMilliseconds(jitterer.Next(0, 1000)) // Random jitter
+))
+.AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().CircuitBreakerAsync(
+	3, // no of failing requests before breaking the circuit
+	TimeSpan.FromSeconds(15) // time we are going to keep our circuit open
 ))
 .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(TimeSpan.FromSeconds(1)));
 
